a3a888d525713ee62f116a29842d83f07dceecaf
Brooke Wilder <brookewilder19@gmail.com>
2022-03-02 10:28:35 -0500

    display all pies better

--- client/components/SinglePieItem.js
+++ client/components/SinglePieItem.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom'
 
 //single pies that show up in featured and on all pies view
 export default function SinglePieItem(props) {
+    console.log(props.pie)
     return(
         <div className='single-pie-item'>
-            <Link to= {`/pies/${props.pie.id}`}>{props.pie.name}</Link>
+            <Link to= {`/pies/${props.pie.id}`}><img style={{width: '200px'}} src={props.pie.thumbnailurl}/></Link>
+            <h3 className='item-pie-name'>{props.pie.name}</h3>
             
         </div>
     )
