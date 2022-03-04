62b3eb2b7c9ea01e25fd58238bd2fb6b21f75f16
Sarina Chang <sarinachang7@gmail.com>
2022-03-04 14:34:24 -0500

    all/single page  css/buttons

--- client/components/SinglePieItem.js
+++ client/components/SinglePieItem.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 //single pies that show up in featured and on all pies view
 export default function SinglePieItem(props) {
-    console.log(props.pie)
-    return(
-        <div className='single-pie-item'>
-            <Link to= {`/pies/${props.pie.id}`}><img style={{width: '200px'}} src={props.pie.thumbnailurl}/></Link>
-            <h3 className='item-pie-name'>{props.pie.name}</h3>
-        </div>
-    )
+  console.log(props.pie);
+  return (
+    <div className='single-pie-item'>
+      <Link to={`/pies/${props.pie.id}`}>
+        <img src={props.pie.thumbnailurl} />
+      </Link>
+      <p className='pie-name'>{props.pie.name}</p>
+      <p className='pie-price'>${props.pie.price}</p>
+      <button>X</button>
+    </div>
+  );
 }
--- client/store/singlePie.js
+++ client/store/singlePie.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 // ACTION TYPES:
 const SET_SINGLE_PIE = 'SET_SINGLE_PIE';
 
-
 // ACTION CREATORS:
 export const setSinglePie = (pie) => ({
   type: SET_SINGLE_PIE,
@@ -23,12 +22,12 @@ export const fetchSinglePie = (pieId) => {
 };
 
 export const updatePie = (updatedPie) => {
-  console.log("=======",updatedPie)
+  console.log('=======', updatedPie);
   return async (dispatch) => {
-   const {data} = await axios.put(`/api/pies/${updatedPie.id}`, updatedPie)
-   dispatch(setSinglePie(data));
-  }
-}
+    const { data } = await axios.put(`/api/pies/${updatedPie.id}`, updatedPie);
+    dispatch(setSinglePie(data));
+  };
+};
 
 // INITIAL STATE:
 const initialState = {};
