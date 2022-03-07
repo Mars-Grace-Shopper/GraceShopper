eaf96373789662314871ccd990523547e9158c2e
Mai <jea989@gmail.com>
2022-03-07 03:20:39 -0500

    persistent cart implemented for logged in users

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,6 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import SingleCartRow from "./SingleCartRow";
 
+import axios from 'axios';
+
+
 export class Cart extends React.Component {
   constructor() {
     super();
@@ -16,7 +19,14 @@ export class Cart extends React.Component {
     this.findTotalPrice = this.findTotalPrice.bind(this)
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      const {data} = await axios.get(`/api/cart`,{headers:{authorization: token}})
+      localStorage.setItem('cart', JSON.stringify(data))
+    }
+    
     let localCart = eval(localStorage.getItem("cart"));
     if (!Array.isArray(localCart)) {
       localCart = [];
@@ -38,33 +48,55 @@ export class Cart extends React.Component {
   async handleRemove(id) {
     await this.setState({cart: this.state.cart.filter(i => i.pie.id  != id)})
     localStorage.setItem("cart", JSON.stringify(this.state.cart))
+
+    const token = localStorage.getItem('token')
+    if (token) {
+      await axios.delete(`/api/cart/cartitem/` + id, {headers:{authorization: token}})
+    }
   }
 
   async handleIncrement(id) {
     console.log("Handleincrement: ", id);
     console.log('this.state.cart : ', this.state.cart)
+    let newQty;
+
+    const token = localStorage.getItem('token')
     const tmpCart = this.state.cart
     for (let i = 0; i < tmpCart.length; ++i) {
       if (tmpCart[i]['pie']['id'] === id) {
         tmpCart[i]['quantity'] += 1;
+        newQty = tmpCart[i]['quantity'];
       }
     }
     await this.setState({...this.state, cart: tmpCart})
     localStorage.setItem("cart", JSON.stringify(this.state.cart))
+
+    if (token) {
+      await axios.put(`/api/cart/cartitem`, {quantity: newQty, pieId: id}, {headers:{authorization: token}})
+    }
+
   }
 
   async handleDecrement(id) {
     console.log("HandleDecrement: ", id);
+    let newQty;
+    const token = localStorage.getItem('token')
+  
     const tmpCart = this.state.cart
     for (let i = 0; i < tmpCart.length; ++i) {
       if (tmpCart[i]['pie']['id'] === id) {
         if (tmpCart[i]['quantity'] > 1) {
           tmpCart[i]['quantity'] -= 1;
+          newQty = tmpCart[i]['quantity'];
         }
       }
     }
     await this.setState({...this.state, cart: tmpCart})
     localStorage.setItem("cart", JSON.stringify(this.state.cart))
+
+    if (token) {
+      await axios.put(`/api/cart/cartitem`, {quantity: newQty, pieId: id}, {headers:{authorization: token}})
+    }
   }
 
   findTotalQuantity(cart) {
--- server/api/cart.js
+++ server/api/cart.js
@@ -1,15 +1,82 @@
 const router = require('express').Router();
 const {
-    models: { Cart, CartItem },
+    models: { Cart, CartItem ,Pie},
 } = require('../db');
+const {requireUserToken} = require('./gatekeeper')
 
 module.exports = router;
 
 // GET /api/cart/
-router.get('/', async (req, res, next) => {
-    try {
-     
-    } catch (err) {
-      next(err);
+router.get('/', requireUserToken, async(req, res, next) => {
+  try {
+    if(req.user){
+    const user = req.user;
+    const [cart] = await user.getCarts({where: {paid: false}})
+    const cartItems = await cart.getCartitems();
+    const data = await Promise.all(cartItems.map(async (item) => {
+      let pie = await Pie.findByPk(item.pieId);
+      return {id: item.id, quantity: item.quantity, pie: pie}
+    }))
+    res.send(data)
+  }
+  res.send()
+  } catch (error) {
+    
+  }
+})
+
+// POST /api/cart/cartitem
+router.post('/cartitem', requireUserToken, async(req, res, next) => {
+  try {
+    if(req.user) {
+      const user = req.user;
+      const [cart] = await user.getCarts({where: {paid: false}})
+      await cart.createCartitem({pieId: req.body.pieId, quantity: req.body.quantity})
+//      const [cartitem] = await cart.getCartitems({where: {pieId: req.body.pieId}});
+      //console.log(cartitem.quantity)
+//      cartitem.update({quantity: req.body.quantity})
+//      cartitem.save()
+      //console.log('cartitem.quantity', cartitem.quantity)
+      res.status(204).end();
     }
-});
+  } catch (error) {
+    next(error)
+  }
+})
+
+// PUT /api/cart/cartitem
+router.put('/cartitem', requireUserToken, async(req, res, next) => {
+  try {
+    if(req.user) {
+      const user = req.user;
+      const [cart] = await user.getCarts({where: {paid: false}})
+      const [cartitem] = await cart.getCartitems({where: {pieId: req.body.pieId}});
+      //console.log(cartitem.quantity)
+      cartitem.update({quantity: req.body.quantity})
+      cartitem.save()
+      //console.log('cartitem.quantity', cartitem.quantity)
+      res.status(204).end();
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+// DELETE /api/cart/cartitem
+router.delete('/cartitem/:pieId', requireUserToken, async(req, res, next) => {
+  try {
+    if(req.user) {
+      const user = req.user;
+      const [cart] = await user.getCarts({where: {paid: false}})
+      const [cartitem] = await cart.getCartitems({where: {pieId: req.params.pieId}});
+      await cart.removeCartitems(cartitem.id);
+
+      res.status(204).end();
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+
