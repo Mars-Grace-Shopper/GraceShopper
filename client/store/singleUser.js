584c400fa1990f7c402d36d249c6e7a72c17d637
Joanne <jl7105@nyu.edu>
2022-03-07 11:15:45 -0500

    merging updates

---
+++ client/store/singleUser.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+// ACTION TYPES:
+const SET_SINGLE_PIE = 'SET_SINGLE_PIE';
+
+// ACTION CREATORS:
+export const setSinglePie = (pie) => ({
+  type: SET_SINGLE_PIE,
+  pie,
+});
+
+// THUNK CREATORS:
+export const fetchSinglePie = (pieId) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/pies/${pieId}`);
+      dispatch(setSinglePie(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const updatePie = (updatedPie) => {
+  return async (dispatch) => {
+    try {
+      const token = window.localStorage.getItem('token');
+      if (token) {
+        const { data } = await axios.put(
+          `/api/pies/${updatedPie.id}`,
+          updatedPie,
+          {
+            headers: {
+              authorization: token,
+            },
+          }
+        );
+        dispatch(setSinglePie(data));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+// INITIAL STATE:
+const initialState = {};
+
+// SUB-REDUCER:
+export default function singlePieReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_SINGLE_PIE:
+      return action.pie;
+    default:
+      return state;
+  }
+}
--- server/api/users.js
+++ server/api/users.js
@@ -18,3 +18,28 @@ router.get('/', requireAdminToken,  async (req, res, next) => {
     next(err)
   }
 })
+
+// -------------- HAVE TO REFACTOR BELOW -------------
+
+// GET /api/userhome/:id
+router.get('userhome/:id', async (req, res, next) => {
+  try {
+    const pie = await Pie.findByPk(req.params.id);
+    res.json(pie);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /api/userhome/:id to update a user
+router.put('userhome/:id', requireAdminToken, async (req, res, next) => {
+  try {
+    if (!req.admin) throw new Error('Unauthorized');
+    const id = req.params.id;
+    const pie = await Pie.findByPk(id);
+    await pie.update(req.body);
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
