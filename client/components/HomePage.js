e61bb0f461aca53ac9b18c7204ec383583e63264
Sarina Chang <sarinachang7@gmail.com>
2022-03-07 17:22:02 -0500

    confirmation page created

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -15,10 +15,10 @@ export default class HomePage extends Component {
           <div className='intro-para'>
             <p>
               Your source for delicious pies from around the world, brought to you by <span data-hover="Brooke, Joanne, Mai, and Sarina!">Team Mars</span>!<br/>
-              Experience only the finest, first-rate, five-star, Gordon Ramsey himself approved pies from a myriad of different places and cultures.<br/><br/>
+              Experience only the finest, first-rate, five-star, the-man-Gordon-Ramsey-himself approved pies from a myriad of different places and cultures.<br/><br/>
               We are a company based in the U.S. even though our pies are shipped from their respective countries for the most authentic experience; hence the prices so don't question it. We are just that good trust us fam fr lmao<br /><br />
               Not to be confused with IHOP&#174; as we are better than them. No we do not take criticism.<br /><br />
-              <span data-hover="yall are cute af">Enjoy your stay! &#x2665;</span>
+              <span data-hover="whoever reading this owe me $5">Enjoy your stay! &#x2665;</span>
             </p>
           </div>
         </div>
