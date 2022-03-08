8d222270cb940eaa9c0c5e67beda6940f1ba22ea
Mai <jea989@gmail.com>
2022-03-08 15:34:38 -0500

    make view order button work on confirmation page

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -4,8 +4,14 @@ import { Link } from 'react-router-dom';
 
 // order.updatedAt.slice(0, 10)
 
+
 export default function Confirmation(props) {
   console.log('CONF props:', props)
+
+  const handleVO = () => {
+        this.props.history.push({pathname: "/order/" + props.location.state.orderId, state: {orderId: props.location.state.orderId, orderDate: props.location.state.orderDate}});
+  }
+
   return (
     <div className='confirmation'>
       <div className='confirmation-msg'>
@@ -17,7 +23,9 @@ export default function Confirmation(props) {
           Order # <Link to={{pathname:'/order/' + props.location.state.orderId, state: {orderId: props.location.state.orderId, orderDate: props.location.state.orderDate} }} ><span style={{ color: '#3961e7' }}>{props.location.state.orderId}</span></Link>
         </p>
       </div>
-      <button>VIEW ORDER</button>
+      <Link to={{pathname:'/order', state: {orderId: props.location.state.orderId, orderDate: props.location.state.orderDate} }} >
+        <button>VIEW ORDER</button>
+      </Link>
       <Link to='/pies'>
         <button>CONTINUE SHOPPING</button>
       </Link>
